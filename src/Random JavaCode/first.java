22119965ca5fa1aa34b746f0af5b6a666ffaef59
MDalamin5 <mdal.amin5@northsouth.edu>
2023-04-25 22:14:36 +0600

    Java practice

--- src/Random JavaCode/first.java
+++ src/Random JavaCode/first.java
@@ -4,5 +4,10 @@ public class first
     public static void main(String [] args)
     {
         System.out.println("Asslamulaikem");
+        System.out.println("Hissss");
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n=scan.nextInt();
+        System.out.println(n*n);
     }
 }
