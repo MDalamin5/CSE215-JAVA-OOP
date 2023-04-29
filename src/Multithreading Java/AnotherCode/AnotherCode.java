be6267416affec415ea445f6bade23b1da44674c
MDalamin5 <mdal.amin5@northsouth.edu>
2023-04-29 22:44:56 +0600

    thread

--- src/Multithreading Java/AnotherCode/AnotherCode.java
+++ src/Multithreading Java/AnotherCode/AnotherCode.java
@@ -1,6 +1,21 @@
 
-
-public class AnotherCode {
-    
+//import java.lang.*;
+public class AnotherCode implements Runnable {
+  
+    public void run()
+    {
+        for(int i=0;i<10;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        AnotherCode ob = new AnotherCode();
+        Thread th = new Thread(ob);
+        th.start();
+    }
     
 }
